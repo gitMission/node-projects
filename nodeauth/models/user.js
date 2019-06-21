7d0b07463d4b813874ba7e65fc725ad8557bada2
gitMission <missionrenjr@gmail.com>
2019-06-21 17:08:35 +0800

    upload nodeauth

---
+++ nodeauth/models/user.js
@@ -0,0 +1,53 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+
+mongoose.connect('mongodb://localhost/nodeauth', { useNewUrlParser: true });
+
+var db = mongoose.connection;
+
+//User Schema
+var UserSchema = mongoose.Schema({
+    username: {
+        type: String,
+        index: true
+    },
+    password: {
+        type: String,
+    },
+    email: {
+        type: String,
+    },
+    name: {
+        type: String,
+    },
+    profileimage: {
+        type: String,
+    }
+});
+
+var User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserById = (id, callback) => {
+    User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = (username, callback) => {
+    var query = { username: username };
+    User.findOne(query, callback);
+}
+
+module.exports.comparePassword = (candidatePassword, hash, callback) => {
+    bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+        callback(null, isMatch);
+    });
+}
+
+module.exports.createUser = (newUser, callback) => {
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(newUser.password, salt, function(err, hash) {
+            newUser.password = hash;
+            newUser.save(callback);
+        });
+    });
+    newUser.save(callback);
+}
